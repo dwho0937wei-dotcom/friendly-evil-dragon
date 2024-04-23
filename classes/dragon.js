9fb8e5fcf00cb82a164bfd2b001273d808302b4a
Daniel Ho <daniel.ho337@gmail.com>
2024-04-23 14:31:14 -0700

    dragon-spec

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,24 @@
 // Your code here 
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
 
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(dragons) {
+    return dragons.map(dragon => dragon.name);
+  }
+}
+
+let dragon1 = new Dragon('Timmy Turner', 'pink');
+let dragon2 = new Dragon("Puff", "green");
+let dragon3 = new Dragon("Toothless", "black");
+
+console.log( Dragon.getDragons([dragon1, dragon2, dragon3]) );
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
