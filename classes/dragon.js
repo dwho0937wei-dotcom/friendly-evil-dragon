a90b04bead661e502334d589252b00649984839b
Mucheng Ruan <rmumu029@gmail.com>
2024-04-23 14:52:14 -0700

    friendly

--- classes/dragon.js
+++ classes/dragon.js
@@ -9,7 +9,7 @@ class Dragon {
     return `${this.name} breathes fire everywhere! BURN!!!!`;
   }
 
-  static getDragons(dragons) {
+  static getDragons(...dragons) {
     return dragons.map(dragon => dragon.name);
   }
 }
@@ -18,7 +18,8 @@ let dragon1 = new Dragon('Timmy Turner', 'pink');
 let dragon2 = new Dragon("Puff", "green");
 let dragon3 = new Dragon("Toothless", "black");
 
-console.log( Dragon.getDragons([dragon1, dragon2, dragon3]) );
+console.log( Dragon.getDragons(dragon1, dragon2,dragon3) );
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,12 @@
-// Your code here 
+const Dragon = require('./dragon');
+
+
+class FriendlyDragon extends Dragon {
+    constructor(name, color, lifeGoals, friend) {
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+}
+const friendly1 = new FriendlyDragon('Timmy Turner', 'pink', 'fight', 'Puff');
+console.log(friendly1);
