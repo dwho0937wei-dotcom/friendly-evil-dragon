507c8ae0636cf02449729336fd7d9d5f84db8ae7
Mucheng Ruan <rmumu029@gmail.com>
2024-04-23 15:42:38 -0700

    evilDoing

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -4,6 +4,29 @@ const Dragon = require('./dragon');
 class EvilDragon extends Dragon {
     constructor(name, color, evilDoings, nemesis) {
         super(name, color);
-        
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        this.evilDoings.forEach(doing => console.log(`${this.name} will ${doing}`))
     }
 }
+
+
+
+const enderDragon = new EvilDragon(
+    "Ender Dragon",
+    "black", [
+      "spit a fireball at Steve",
+      "rule over all the Endermen",
+      "destroy all blocks in its way"
+    ],
+    "Steve"
+  );
+  console.log(enderDragon);
+  enderDragon.dontInviteThemOverForDinner();
+  console.log(enderDragon.breathesFire());
+  console.log(enderDragon.burnsNemesis());
+
+module.export = EvilDragon;
